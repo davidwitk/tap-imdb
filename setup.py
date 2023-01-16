c8bf897a6d1e560d38b9f8e93e3d15b5377aa07f
David Witkowski <43667775+chodera@users.noreply.github.com>
2023-01-16 12:22:21 +0100

    Make it a working package

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ from setuptools import setup
 
 setup(
    name='tap-imdb',
-   version='0.0.1',
+   version='0.1.0',
    description='A python module to train creating packages for Singer Taps based on IMDb data',
    author='David Witkowski',
+	classifiers=["Programming Language :: Python :: 3 :: Only"],
    install_requires=[
       'beautifulsoup4==4.11.1', 
       'requests==2.28.1', 
       'singer-python==5.13.0'
-   ]
+   ],
+   entry_points={
+      'console_scripts': ['tap-imdb=tap_imdb:main']
+   }
 )
--- tap_imdb/__init__.py
+++ tap_imdb/__init__.py
@@ -1,4 +1,7 @@
-from sync import sync_imdb_top_250
+from tap_imdb.sync import sync_imdb_top_250
+
+def main():
+    sync_imdb_top_250()
 
 if __name__ == "__main__":
-    sync_imdb_top_250()
+    main()
