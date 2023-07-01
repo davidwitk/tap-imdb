0845812ac80a5bd4b52a5c9a418c27a357114080
David Witkowski <43667775+chodera@users.noreply.github.com>
2023-07-01 10:33:27 +0200

    Implement retry and switch to Firefox driver

--- tap_imdb/sync.py
+++ tap_imdb/sync.py
@@ -2,37 +2,55 @@ import bs4
 import datetime
 import singer
 import hashlib
+import logging
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 import time
 
 def get_imdb_top_250():
     
-    # Set Chrome webdriver
-    options = Options()
-    options.add_argument("--headless")
-    driver = webdriver.Chrome(options=options)  
-    
-    # Request page
-    driver.get('https://www.imdb.com/chart/top/')
+    # IMDb seems to expose the new page design randonmly based on an AB-Test. In some cases, the old page design
+    # might still appear. In this case, we simply try again to get the new page design.
+    i = 0
+    while i < 10:     
+        i += 1
 
-    # Select detailed view (this is why we actually use Selenium now)
-    driver.find_element(By.ID, 'list-view-option-detailed').click()
+        try: 
+            logging.info('Setting Firefox driver.')
+            
+            # Set Firefox webdriver
+            options = Options()
+            options.add_argument("--headless")
+            options.set_preference('intl.accept_languages', 'en-US, en')
+            driver = webdriver.Firefox(options=options)
 
-    # Scroll to bottom of the page due to dynamic page loading
-    SCROLL_PAUSE_TIME = 0.5
-    last_height = driver.execute_script("return document.body.scrollHeight") # Get scroll height
-    while True:
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") # Scroll down to bottom
-        time.sleep(SCROLL_PAUSE_TIME) # Wait to load page
-        new_height = driver.execute_script("return document.body.scrollHeight") # Calculate new scroll height and compare with last scroll height
-        if new_height == last_height:
+            # Request page
+            logging.info(f'Requesting the website and trying to parse the HTML - attempt {i}.')
+            driver.get('https://www.imdb.com/chart/top/')
+
+            # Select detailed view (this is why we actually use Selenium now)
+            driver.find_element(By.ID, 'list-view-option-detailed').click()
+
+            # Scroll to bottom of the page due to dynamic page loading
+            SCROLL_PAUSE_TIME = 0.5
+            last_height = driver.execute_script("return document.body.scrollHeight") # Get scroll height
+            while True:
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") # Scroll down to bottom
+                time.sleep(SCROLL_PAUSE_TIME) # Wait to load page
+                new_height = driver.execute_script("return document.body.scrollHeight") # Calculate new scroll height and compare with last scroll height
+                if new_height == last_height:
+                    break
+                last_height = new_height
+
+            soup = bs4.BeautifulSoup(driver.page_source, 'html.parser')
+
+            driver.close()
+
             break
-        last_height = new_height
 
-    soup = bs4.BeautifulSoup(driver.page_source, 'html.parser')
-    driver.close()
+        except Exception as e:
+            logging.info('Oops! That was not the new page design. Try again...')
 
     return soup 
 
