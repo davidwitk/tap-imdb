41aaaebf8c751019e632a6726f245eb2d2ccb520
David Witkowski <david.witkowski@posteo.net>
2023-10-14 22:09:10 +0100

    Do not use unstable class IDs for selection

--- tap_imdb/sync.py
+++ tap_imdb/sync.py
@@ -64,10 +64,10 @@ def sync_imdb_top_250():
     for movie in movies_soup: 
 
         title = movie.find(class_='ipc-title__text').get_text().split('.', 1)[1].strip()
-        year = movie.find_all(class_='sc-6fa21551-8')[0].get_text()
+        year = movie.find('span', class_='dli-title-metadata-item').text
         rank = movie.find(class_='ipc-title__text').get_text().split('.', 1)[0]
         rating = movie.find("span", class_="ipc-rating-star").get_text(strip=True).split('(', 1)[0]
-        rating_count = movie.find("div", class_="sc-db4d5a92-0").get_text().replace('Votes','').replace(',','').replace('.','')
+        rating_count = movie.find('span', text='Votes').find_next_sibling(text=True).strip().replace(',','')
         link = 'https://imdb.com' + movie.select("a", class_='ipc-title-link-wrapper')[0]['href'].split('?')[0]
         id = hashlib.md5((title + extracted_at).encode('utf-8')).hexdigest() 
 
